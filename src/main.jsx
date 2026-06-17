eca2c22bd4c2594a23d0ec57270d69e393caf0b3
Kabir Ul Althamash P. <kalthamash@zetaglobal.com>
2026-06-17 16:41:55 +0530

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
+    <Analytics />
   </BrowserRouter>
 );
